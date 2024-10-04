4cbc74232371a2647f74e08ee47d9a83c02df594
Pankaj Sharma <pankaj.avology@gmail.com>
2024-10-04 18:38:03 +0530

    complete homepage widt responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 const header_navIcon = document.querySelector(".header_navIcon");
+const navbar = document.querySelector(".sideBar_menu");
+const closeBar = document.querySelector(".close_img");
+
 header_navIcon.addEventListener("click",function(){
-  alert("hello")
+  navbar.classList.add("active")
+})
+
+closeBar.addEventListener("click",function(){
+  navbar.classList.remove("active")
 })
 
 // slick slider code
@@ -9,14 +16,36 @@ $('.center').slick({
   centerPadding: '51px',
   slidesToShow: 4,
   autoplay: true,
+  speed: 500,
+  autoplaySpeed: 1000,
   responsive: [
+  ],
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '51px',
+        slidesToShow: 4 
+      }
+    },
+    {
+      breakpoint: 998,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '45px',
+        slidesToShow: 3 
+      }
+    },
     {
       breakpoint: 768,
       settings: {
         arrows: true,
         centerMode: true,
-        centerPadding: '50px',
-        slidesToShow: 5 
+        centerPadding: '40px',
+        slidesToShow: 2 
       }
     },
     {
@@ -24,21 +53,10 @@ $('.center').slick({
       settings: {
         arrows: true,
         centerMode: true,
-        centerPadding: '40px',
+        centerPadding: '10px',
         slidesToShow: 1
       }
     }
-  ],
-  responsive: [
-    {
-      breakpoint: 1400,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '51px',
-        slidesToShow: 5 
-      }
-    }
-   
   ]
+ 
 });
